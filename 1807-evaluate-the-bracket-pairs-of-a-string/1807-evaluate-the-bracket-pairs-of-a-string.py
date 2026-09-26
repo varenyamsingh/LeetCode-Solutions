88c456abd3bbba9f8c6ba319882763ea6de57a55
Varenyam Singh Baghel <130786405+varenyamsingh@users.noreply.github.com>
2026-09-26 09:09:26 +0530

    Time: 51 ms (57.94%), Space: 51.6 MB (39.25%) - LeetHub

---
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.py
@@ -0,0 +1,25 @@
+class Solution:
+    def evaluate(self, s, knowledge):
+        mp = dict(knowledge)
+        result = []
+
+        i = 0
+
+        while i < len(s):
+            if s[i] == '(':
+                j = i + 1
+
+                while s[j] != ')':
+                    j += 1
+
+                key = s[i + 1:j]
+
+                result.append(mp.get(key, "?"))
+
+                i = j + 1
+
+            else:
+                result.append(s[i])
+                i += 1
+
+        return "".join(result)
